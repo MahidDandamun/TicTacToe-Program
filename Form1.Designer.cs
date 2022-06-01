4481d96fd3f77573f5d3d230c9c52f3e8dc5fdff
MahidDandamun <Dandamunmahid@gmail.com>
2022-06-02 00:03:56 +0800

    Design Done

---
+++ Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Basic_TicTacToe_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScoreX = new System.Windows.Forms.Label();
+            this.lblScoreO = new System.Windows.Forms.Label();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblScoreDraw = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn1.Location = new System.Drawing.Point(4, 4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(88, 82);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "X";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn2.Location = new System.Drawing.Point(92, 4);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(88, 82);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "O";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn3.Location = new System.Drawing.Point(180, 4);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(88, 82);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "O";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn4.Location = new System.Drawing.Point(4, 85);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(88, 82);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "X";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn5.Location = new System.Drawing.Point(92, 85);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(88, 82);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "X";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn6.Location = new System.Drawing.Point(180, 85);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(88, 82);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "X";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn7.Location = new System.Drawing.Point(4, 166);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(88, 82);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "O";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn8.Location = new System.Drawing.Point(92, 166);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(88, 82);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "X";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(21)))), ((int)(((byte)(29)))));
+            this.btn9.Location = new System.Drawing.Point(180, 166);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(88, 82);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "O";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(104, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Player - X ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(285, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Player - O";
+            // 
+            // lblScoreX
+            // 
+            this.lblScoreX.AutoSize = true;
+            this.lblScoreX.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScoreX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(91)))));
+            this.lblScoreX.Location = new System.Drawing.Point(110, 64);
+            this.lblScoreX.Name = "lblScoreX";
+            this.lblScoreX.Size = new System.Drawing.Size(66, 18);
+            this.lblScoreX.TabIndex = 12;
+            this.lblScoreX.Text = "Score1";
+            // 
+            // lblScoreO
+            // 
+            this.lblScoreO.AutoSize = true;
+            this.lblScoreO.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScoreO.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(91)))));
+            this.lblScoreO.Location = new System.Drawing.Point(289, 64);
+            this.lblScoreO.Name = "lblScoreO";
+            this.lblScoreO.Size = new System.Drawing.Size(70, 18);
+            this.lblScoreO.TabIndex = 13;
+            this.lblScoreO.Text = "Score 2";
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblExit.Location = new System.Drawing.Point(439, 4);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(30, 32);
+            this.lblExit.TabIndex = 14;
+            this.lblExit.Text = "X";
+            this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(91)))));
+            this.label3.Location = new System.Drawing.Point(12, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 24);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Tic-Tac-Toe";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Location = new System.Drawing.Point(98, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 252);
+            this.panel1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(208, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 18);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Draws";
+            // 
+            // lblScoreDraw
+            // 
+            this.lblScoreDraw.AutoSize = true;
+            this.lblScoreDraw.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScoreDraw.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(80)))), ((int)(((byte)(91)))));
+            this.lblScoreDraw.Location = new System.Drawing.Point(202, 64);
+            this.lblScoreDraw.Name = "lblScoreDraw";
+            this.lblScoreDraw.Size = new System.Drawing.Size(70, 18);
+            this.lblScoreDraw.TabIndex = 18;
+            this.lblScoreDraw.Text = "Score 3";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(62)))), ((int)(((byte)(72)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(178, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 25);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Reset Board";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(25)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(472, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblScoreDraw);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.lblScoreO);
+            this.Controls.Add(this.lblScoreX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Label label1;
+        private Label label2;
+        private Label lblScoreX;
+        private Label lblScoreO;
+        private Label lblExit;
+        private Label label3;
+        private Panel panel1;
+        private Label label4;
+        private Label lblScoreDraw;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace Basic_TicTacToe_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
