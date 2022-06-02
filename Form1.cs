537dc50a1630f238558a754492c94b26c6a9051c
MahidDandamun <Dandamunmahid@gmail.com>
2022-06-02 16:17:07 +0800

    All buttons are working except the reset button and have no player sequencing

--- Form1.cs
+++ Form1.cs
@@ -27,42 +27,74 @@ namespace Basic_TicTacToe_program
 
         private void btn2_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn2.Text = "X";
+            else
+                btn2.Text = "O";
+            btn2.Enabled = false;
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn3.Text = "X";
+            else
+                btn3.Text = "O";
+            btn3.Enabled = false;
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn4.Text = "X";
+            else
+                btn4.Text = "O";
+            btn4.Enabled = false;
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn5.Text = "X";
+            else
+                btn5.Text = "O";
+            btn5.Enabled = false;
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn6.Text = "X";
+            else
+                btn6.Text = "O";
+            btn6.Enabled = false;
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn7.Text = "X";
+            else
+                btn7.Text = "O";
+            btn7.Enabled = false;
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn8.Text = "X";
+            else
+                btn8.Text = "O";
+            btn8.Enabled = false;
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-
+            if (playersequence)
+                btn9.Text = "X";
+            else
+                btn9.Text = "O";
+            btn1.Enabled = false;
         }
     }
 }
